0d8ea6bb57b83a11438d0e385fc9a0de7b3bdf90
edavidaja <david@rstudio.com>
2022-11-14 20:38:25 +0000

    change permissions on executable before running

--- plumber.R
+++ plumber.R
@@ -4,13 +4,15 @@ library(fastmap)
 library(promises)
 future::plan("multicore")
 
+
 #* run SS
 #* @get /ss
 function() {
   future_promise(
-   system(command = "./ss")
+    system(command = "chmod +x ./ss && ./ss")
   )
 }
+
 
 #* get SS results
 #* @get /results
